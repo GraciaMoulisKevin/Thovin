ff5c46c44b45d684aeb100443ad19e8bde0515be
Gracia-Moulis Kevin <kevin.gracia-moulis@etu.umontpellier.fr>
2021-03-29 18:42:13 +0200

    activité profil, editProfil, panier

--- app/src/main/java/com/example/thovin/Panier.java
+++ app/src/main/java/com/example/thovin/Panier.java
@@ -2,13 +2,52 @@ package com.example.thovin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 public class Panier extends AppCompatActivity {
+
+    private Button panier, profil, cancel, validate;
+    private ScrollView articles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panier);
+
+        articles = (ScrollView) findViewById(R.id.panier_articles);
+
+        // Chargement de la scroll view
+
+        // Buttons
+        panier = (Button) findViewById(R.id.panier_panier);
+        profil = (Button) findViewById(R.id.panier_profil);
+        cancel = (Button) findViewById(R.id.panier_cancel);
+        validate = (Button) findViewById(R.id.panier_validate);
+
+        validate.setText(""+prixPanier());
+
+        panier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_profil = new Intent(Panier.this, Profil.class);
+                startActivity(intent_profil);
+            }
+        });
+    }
+
+    private double prixPanier(){
+        // calcul prix du panier selon "articles"
+        return 32.70;
     }
 }
